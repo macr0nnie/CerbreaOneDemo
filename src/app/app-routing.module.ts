1aef0423991687e2bee940b07ed6411d2a3f640f
macronnie <macr0nnieasmr@gmail.com>
2025-06-10 13:18:03 -0400

    adding more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PreferencesComponent } from './components/preferences/preferences.component';
+import { ClientsComponent } from './components/clients/clients.component';
+
 
 const routes: Routes = [
-  
+   {path:'dashboard', component: DashboardComponent},
+   {path: 'preferences',component: PreferencesComponent},
+   {path: 'clients', component: ClientsComponent}, // Adjust this if ClientsModule is a module or component
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { 
+  
+
 
 }
