85afe79a2190cf206c05dd5c55c5c536ee3d0741
macronnie <macr0nnieasmr@gmail.com>
2025-06-17 14:44:38 -0400

    removed the collapsing sidebar and added a new service for user data

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//mock implementation for fake users to test the table with
+export class UsersService {
+  constructor() { 
+    
+  }
+}
